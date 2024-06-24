d5ecc22a8ac719b6e9c881a15c95efaafcf38d97
Etienne Pogoda <pogoda.etienne@outlook.fr>
2024-06-24 13:53:20 +0200

    first commit

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public final class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -20,5 +20,47 @@ public class Eagle extends Bird {
     @Override
     public String sing() {
         return "Screech!";
+    }
+
+    @Override
+    public void takeOff() {
+        if (!this.flying && this.altitude == 0) {
+            this.flying = true;
+            System.out.printf("%s takes off in the sky.%n", this.getName());
+        }
+    }
+
+    @Override
+    public void glide() {
+        if (this.flying) {
+            System.out.printf("glides into the air. %n");
+        }
+    }
+
+    @Override
+    public void land() {
+        if (this.flying && this.altitude == 0) {
+            System.out.printf("%s lands on the ground.%n", this.getName());
+        } else {
+            System.out.printf("%s is too high, it can't land.%n", this.getName());
+        }
+    }
+
+    @Override
+    public int ascend(int alt) {
+        if (this.flying) {
+            this.altitude = Math.min(this.altitude + alt, 1000);
+            System.out.printf("%s flies upward, altitude : %d%n", this.getName(), this.altitude);
+        }
+        return altitude;
+    }
+
+    @Override
+    public int descend(int alt) {
+        if (this.flying) {
+            this.altitude = Math.min(this.altitude - alt, 1000);
+            System.out.printf("%s flies downward, altitude : %d%n", this.getName(), this.altitude);
+        }
+        return altitude;
     }
 }
